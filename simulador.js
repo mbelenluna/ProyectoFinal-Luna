4381700f4e411be93cf2863dc7258695b304a6c2
mbelenluna <mariabelenluna18@gmail.com>
2022-07-28 23:59:25 -0300

    First push

---
+++ simulador.js
@@ -0,0 +1,7 @@
+//Variables
+
+wordcount = parseInt(prompt("Please insert the word count of your file(s)."));
+sourceLanguage = prompt("Please insert the source language.");
+targetLanguage = prompt("Which language do you need your document translated into?");
+turnaround = prompt("How soon do you need it?");
+
