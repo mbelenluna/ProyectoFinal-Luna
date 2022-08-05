e2a256f7e76495313064f3cd61b809f0b2f761f2
mbelenluna <mariabelenluna18@gmail.com>
2022-08-04 22:44:29 -0300

    Arrays

--- simulador.js
+++ simulador.js
@@ -1,60 +1,74 @@
 //Creo función para calcular el monto final
 
 function calculatePrice() {
-    return wordcount * rate;
+    return totalWordcount * rate;
 }
 
 //Variables
 
-let wordcount = parseInt(prompt("Please insert the word count of your file(s)."));
+let wordcount;
+let addFiles = true;
+let rate = 0;
+let totalAmount = 0;
+let totalWordcount = 0;
+const fileNames = [];
+
+while (addFiles === true) { 
+    wordcount = parseInt(prompt("Please insert the word count of your file."));
+    fileNames.push(prompt("Insert the name of your file."));
+    totalWordcount = totalWordcount + wordcount;
+    addMoreFiles = prompt("Would you like to add more files?");
+    
+    if (addMoreFiles === "yes") {
+        addFiles = true;
+    } else if (addMoreFiles === "no") {
+        addFiles = false;
+    } 
+}
+
 let sourceLanguage = prompt("Please insert the source language.");
 let targetLanguage = prompt("Which language do you need your document translated into?");
 let turnaround = prompt("How soon do you need it?");
-let rate = 0;
-let totalAmount = 0;
 
-//Tasa por palabra según el idioma objetivo
+//Tasa por palabra 
 
-switch(targetLanguage) {
-    case "spanish":
-    case "portuguese":
-        rate = 0.10;
-        break;
-    case "simplified chinese":
-    case "traditional chinese":
-    case "korean":
-    case "french":
-    case "hindi":
-    case "tagalog":
-        rate = 0.13;
-        break;
-    case "arabic":
-    case "italian":
-    case "armenian":
-    case "cambodian":
-    case "farsi":
-    case "polish":
-    case "russian":
-        rate = 0.14;
-        break;
-    case "japanese":
-    case "burmese":
-    case "indonesian":
-    case "german":
-    case "pashto":
-        rate = 0.16;
-        break;
-    case "afrikaans":
-    case "amharic":
-    case "croatian":
-    case "dari":
-    case "dutch":
-    case "swedish":
-        rate = 0.18;
-        break;
-    case "hmong":
-        rate = 0.20;
-        break;
+const services = [
+	{
+		source: "english",
+		target: "spanish",
+		rate: 0.10
+	},
+	{
+		source: "spanish",
+		target: "english",
+		rate: 0.12
+	},
+	{
+		source: "english",
+		target: "simplified chinese",
+		rate: 0.13
+	},
+	{
+		source: "simplified chinese",
+		target: "english",
+		rate: 0.15
+	},
+	{
+		source: "english",
+		target: "traditional chinese",
+		rate: 0.13
+	},
+	{
+		source: "traditional chinese",
+		target: "english",
+		rate: 0.15
+	}
+];
+
+for (const service of services) {
+	if (service.source === sourceLanguage && service.target === targetLanguage) {
+		rate = service.rate;
+	}
 }
 
 //Establezco tarifa mínima
@@ -74,4 +88,4 @@ if (turnaround == "As soon as possible") {
 //Muestro los resultados por alert/consola
 
 alert("The total amount would be $" + totalAmount);
-console.log("Translation from " + sourceLanguage + " into " + targetLanguage + "\nWordcount: " + wordcount + "\nTurnaround: " + turnaround);
+console.log("Translation from " + sourceLanguage + " into " + targetLanguage + "\nWordcount: " + totalWordcount + "\nFile names: " + fileNames.toString() + "\nTurnaround: " + turnaround);
